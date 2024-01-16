f05514df96dc435e9035ac5d8227104957dbb08e
Nidhi0063 <126048657+Nidhi0063@users.noreply.github.com>
2024-01-16 15:19:06 +0530

    Update MovieInfoServiceApplication.java

--- movie-Info-service/src/main/java/io/microservices/movieInfoservice/MovieInfoServiceApplication.java
+++ movie-Info-service/src/main/java/io/microservices/movieInfoservice/MovieInfoServiceApplication.java
@@ -2,10 +2,14 @@ package io.microservices.movieInfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class MovieInfoServiceApplication {
-
+         @Bean// just executes once and acts as producer
+    public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
 	}
